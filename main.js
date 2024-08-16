04f054198d5af217aa890a8928f731e529ab225a
whalebab <juncoder94@gmail.com>
2024-08-16 23:58:04 +0900

    node.js exports와 module.exports

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ let m = require('./math-tools'); // require : 모듈을 load 해주는 함수 (�
                             // . (현재 디렉토리)
 
 console.log(m.PI);
-console.log(m.author);
-console.log(m.add(1, 2));
-console.log(m.test.date);
-console.log(m.test.types);
-m.test.printTypes();
+console.log(m.add(2, 4));
+console.log(m.subtract(1, 2));
+console.log(m.multiply(1, 2));
+console.log(m.divide(1, 2));
--- math-tools.js
+++ math-tools.js
@@ -1,26 +1,11 @@
-const PI = 3.14;
-let author = 'codeIt teacher';
-
-function add(a, b) {
-    return a + b;
-}
-
-let test = {
-    date: '2020-09-20',
-    types: ['safetyTest', 'performanceTest'],
-    printTypes() {
-        for (const i in this.types) {
-            console.log(this.types[i]);
-        }
-    }
+// 객체 만들기
+let calculator = {
+    PI: 3.14,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
 };
 
-// exports : 내보내다
-
-exports.PI = PI;
-exports.author = author;
-exports.add = add;
-exports.test = test;
-
-// exports.add = add;
-// 모듈 외부로 공개할 이름 = 모듈 내부에서의 이름
+// 객체를 모듈로 만들어서 exports
+module.exports = calculator;
